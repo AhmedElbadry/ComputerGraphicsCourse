d2f750989511edd4fa01ed2b4b790a1f95c321d5
Ahmed El Badry <ahmed2lbadry@gmail.com>
2018-10-06 22:51:02 +0200

    random colors for each verex

--- week__01/assignment__01/assignment__01/main.cpp
+++ week__01/assignment__01/assignment__01/main.cpp
@@ -1,13 +1,17 @@
 
 #include <iostream>
-#include <stdlib.h> //Needed for "exit" function
+#include <stdlib.h> //Needed for "exit" function and random
 #include <glut.h>
 #include <math.h>
 #define PI 3.14159265
 const int WINDOW_WIDTH = 600;
 const int WINDOW_HEIGHT = 600;
+const int MAX_SIDES_NUM = 100;
+const int CLR_PARAM = 3;
 
-int NumOfSides = 4;
+int NumOfSides;
+
+float randomClrs[MAX_SIDES_NUM][CLR_PARAM];
 
 using namespace std;
 
@@ -16,6 +20,8 @@ void drawPolygon(int numOfEdges);
 void drawScene();
 void resize(int width, int height);
 void mouse(int button, int state, int x, int y);
+void init();
+void generateClrs();
 
 int main(int argc, char** argv) {
 	//Initialize GLUT
@@ -23,6 +29,8 @@ int main(int argc, char** argv) {
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 	glutInitWindowPosition(100, 100);
 	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT); //Set the window size
+
+	init();
 
 	//Create the window
 	glutCreateWindow("Assignment 1");
@@ -65,7 +73,7 @@ void drawPolygon(int numOfSides) {
 		cout << "x" << i << " = " << currX << endl;
 		cout << "y" << i << " = " << currY << endl;
 		*/
-
+		glColor3f(randomClrs[i][0], randomClrs[i][1], randomClrs[i][2]);
 		glVertex2f(currX, currY);
 
 	}
@@ -81,26 +89,37 @@ void drawScene() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	drawPolygon(NumOfSides);
-	/*
-	glBegin(GL_POLYGON);
-
-	glVertex2f(0.0f, 0.5f);
-	glVertex2f(0.5f, 0.0f);
-	glVertex2f(-0.5f, 0.0f);
-
-	glEnd();*/
 
 	glutSwapBuffers(); //Send the 3D scene to the screen
 }
 
 void mouse(int button, int state, int x, int y) {
 	if (button == GLUT_LEFT_BUTTON && state == GLUT_UP) {
-		NumOfSides++;
+		if (NumOfSides < MAX_SIDES_NUM) {
+			NumOfSides++;
+			generateClrs();
+		}
+			
+
 		cout << NumOfSides << endl;
 	}
 }
 
+void init() {
+	NumOfSides = 4;
 
+	generateClrs();
+
+}
+
+void generateClrs() {
+	
+	for (int i = 0; i < NumOfSides; i++) {
+		for (int j = 0; j < CLR_PARAM; j++) {
+			randomClrs[i][j] = (rand() % 1000) / 1000.0;
+		}
+	}
+}
 void resize(int width, int height) {
 	glutReshapeWindow(WINDOW_WIDTH, WINDOW_HEIGHT); // don't resize the window 
 }
